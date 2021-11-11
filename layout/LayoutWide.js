537913611a41eef839d5aa06f77ca0695ece4293
Solring Lin <sherry12714@gmail.com>
2021-11-11 16:57:47 +0000

    layout: fix layout height for wide screen.

--- layout/LayoutWide.js
+++ layout/LayoutWide.js
@@ -4,10 +4,10 @@ export default function LayoutPhone(props) {
   const {header, children} = props
   return (
     <div className="row g-0 vh-100">
-        <div className="col-md-3">
+        <div className="col-md-3 h-100">
           {header}
         </div>
-        <div className="col-md-9">
+        <div className="col-md-9 h-100">
           {children}
         </div>
       </div>
