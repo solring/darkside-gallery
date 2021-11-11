694353b0df3a560770766dc8569450dc09087eec
Solring Lin <sherry12714@gmail.com>
2021-11-11 23:21:12 +0000

    use next/head instead for meta and links.

--- pages/_ducument.js
+++ pages/_ducument.js
@@ -8,18 +8,6 @@ export default class CostomDocument extends Document {
         <Head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#000000" />
-          <meta
-            name="description"
-            content="personal gallery built on Next.js"
-          />
-
-          <link rel="icon" href="favicon.png" />
-          <link rel="manifest" href="manifest.json" />
-
-          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-
-          <title>Rabbit Hole: Personal Gallery</title>
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-import MainScreen from "../components/MainScreen";
+import React from 'react'
+import Head from 'next/head'
+
+import MainScreen from "../components/MainScreen"
 
 export default function Index() {
-  return <MainScreen />
+  return (
+    <React.Fragment>
+      <Head>
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="personal gallery built on Next.js"
+        />
+
+        <link rel="manifest" href="manifest.json" />
+
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+
+        <title>Rabbit Hole: Personal Gallery</title>
+      </Head>
+      <MainScreen />
+    </React.Fragment>
+  )
 }
